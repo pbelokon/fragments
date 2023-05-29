a5c20fa8957cea6509ef7caf506188a372625ca2
pbelokon <pbelokon@myseneca.ca>
2023-05-29 12:19:06 -0400

    removed authentication.js and added authentication opritons

--- src/authorization/cognito.js
+++ src/authorization/cognito.js
@@ -8,7 +8,7 @@ const passport = require('passport');
 const BearerStrategy = require('passport-http-bearer').Strategy;
 const { CognitoJwtVerifier } = require('aws-jwt-verify');
 
-const logger = require('./logger');
+const logger = require('../logger');
 
 // We expect AWS_COGNITO_POOL_ID and AWS_COGNITO_CLIENT_ID to be defined.
 if (!(process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID)) {
